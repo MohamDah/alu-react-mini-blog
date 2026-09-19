f2bd34f1f6c187511c491a6ebc0ae39ab7ee69f7
MohamDahBS <mohamed.dahab@bestseller.com>
2026-09-19 19:13:46 +0200

    feat: wire up App to render Header and PostList

--- mini-blog/src/App.tsx
+++ mini-blog/src/App.tsx
@@ -1,13 +1,15 @@
-import Header from "./components/Header"
-import PostList from "./components/PostList"
+import Header from "./components/Header";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <>
       <Header />
-      <PostList />
+      <main style={{ padding: "1rem 2rem" }}>
+        <PostList />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
